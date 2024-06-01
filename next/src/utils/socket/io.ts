593b2d25c6a9c747eabcca784c4b43c7adb70830
soravang81 <soravang81@gmail.com>
2024-06-01 13:59:14 +0530

    trying to fix socket on prod

--- next/src/utils/socket/io.ts
+++ next/src/utils/socket/io.ts
@@ -1,10 +1,9 @@
 import { getSession } from 'next-auth/react';
 import { io } from 'socket.io-client';
 
-const socketurl = process.env.SOCKET_URL || "http://localhost:8080";
+const socketurl = process.env.SOCKET_URL || "";
 export const socket = io(socketurl, {
         autoConnect: false,
-        // reconnection : true
     });
 export async function connect(){
   const session = await getSession()
